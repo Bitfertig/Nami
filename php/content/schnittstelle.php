b6f70373d15fc77764f9e4f8bf3e164c528bb05a
Aurel Hermand <github@devone.de>
2015-04-04 18:52:40 +0200

    Schnittstellenbeschreibung aktualisiert

--- php/content/schnittstelle.php
+++ php/content/schnittstelle.php
@@ -3,14 +3,29 @@
 <h1>Schnittstellenbeschreibung</h1>
 
 
+<h2>Abfrage</h2>
 <p>
 	Die Schnittstelle liefert Daten im JSON Format aus. Die Daten können dann auf Ihrem Server zwischengespeichert werden und entweder direkt genutzt werden.
 	Der Link lautet: http://www.servicedomain.de/json.php?id={userid}&amp;key={apikey}
 </p>
 
+
+<h2>Aggregate</h2>
+<p>
+	Aggregate ermöglichen es aus verschiedenen Quellen Daten zu nutzen. Sie müssen zunächst einmal ein Aggregat anlegen unter <a href="?mode=aggregate">Aggregate</a>.
+	Es gibt offene Veranstaltungen und es gibt Veranstaltungen die freigegeben werden müssen, hier müssen Sie die Organisation darum bitten Ihnen die Freigabe zu erteilen.
+	Der Link lautet: http://www.servicedomain.de/json.php?id={userid}&amp;aggregat={aggregatid}&amp;key={apikey}
+</p>
+
+
+<h2>Preise</h2>
 <p>
 	Ein Übertragungsvolumen von 10mb am Tag steht Ihnen kostenfrei zur Verfügung.
 	Wir bieten Ihnen eine Möglichkeit diese kostenfreie Grenze aufzuheben.
 	Das Premium Modell rechnet nur das ab, was Sie tatsächlich an Daten abrufen.
 	Hierbei haben Sie die Möglichkeit eine Obergrenze selbst festzulegen.
 </p>
+
+<div>
+	<a href="?mode=shop">Premiummodell jetzt erwerben!</a>
+</div>
