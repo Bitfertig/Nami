2e4d608a4463a4ef715791c72dca802c6f79a7f7
Aurel Hermand <github@devone.de>
2015-04-05 12:53:01 +0200

    gzip'ing entfernt

--- tools/database.func.php
+++ tools/database.func.php
@@ -1,15 +1,15 @@
 <?php
 
 /**
- * @author awan < http://www.nawa.me >
+ * 
  * 
  */
  
  
 /**
- * Function to build SQL /Importing SQL DATA
+ * Function to import SQL data.
  *
- * @param string $args as the queries of sql data , yopu could use file get contents to read data args
+ * @param string $args as the queries of sql data , you could use file get contents to read data args
  * @param string $dbhost database host
  * @param string $dbuser database user
  * @param string $dbpass database password
@@ -17,72 +17,73 @@
  *
  * @return string complete if complete
  */
-
 function mysqli_import_sql( $args , $dbhost, $dbuser, $dbpass ,$dbname ) {
 
-  // check mysqli extension installed
-  if( ! function_exists('mysqli_connect') ) {
-    die(' This scripts need mysql extension to be running properly ! please resolve!!');
-  }
+	// check mysqli extension installed
+	if ( !function_exists('mysqli_connect') ) {
+		die('This scripts need mysql extension to be running properly! Please resolve!');
+	}
 
-  $mysqli = @new mysqli( $dbhost, $dbuser, $dbpass, $dbname );
-  
-   if( $mysqli->connect_error ) {
-    print_r( $mysqli->connect_error );
-    return false;
-  }
+	$mysqli = @new mysqli( $dbhost, $dbuser, $dbpass, $dbname );
+	
+	 if ( $mysqli->connect_error ) {
+		print_r( $mysqli->connect_error );
+		return false;
+	}
 
-    $querycount = 11;
-    $queryerrors = '';
-    $lines = (array) $args;
-    if( is_string( $args ) ) {
-      $lines =  array( $args ) ;
-    }
+	$querycount = 11;
+	$queryerrors = '';
+	$lines = (array) $args;
+	if ( is_string( $args ) ) {
+		$lines =  array( $args ) ;
+	}
 
-    if ( ! $lines ) {
-      return '' . 'cannot execute ' . $args;
-    }
+	if ( ! $lines ) {
+		return 'cannot execute ' . $args;
+	}
 
-    $scriptfile = false;
-    foreach ($lines as $line) {
-      $line = trim( $line );
-      // if have -- comments add enters
-      if (substr( $line, 0, 2 ) == '--') {
-          $line = "\n" . $line;
-      }
-      if (substr( $line, 0, 2 ) != '--') {
-        $scriptfile .= ' ' . $line;
-        continue;
-      }
-    }
+	$scriptfile = false;
+	foreach ($lines as $line) {
+		$line = trim( $line );
+		// if have -- comments add enters
+		if (substr( $line, 0, 2 ) == '--') {
+				$line = "\n" . $line;
+		}
+		if (substr( $line, 0, 2 ) != '--') {
+			$scriptfile .= ' ' . $line;
+			continue;
+		}
+	}
 
-    $queries = explode( ';', $scriptfile );
-    foreach ($queries as $query) {
-      $query = trim( $query );
-      ++$querycount;
+	$queries = explode( ';', $scriptfile );
+	foreach ($queries as $query) {
+		$query = trim( $query );
+		++$querycount;
 
-      if ( $query == '' ) {
-        continue;
-      }
-
-
-      if ( ! $mysqli->query( $query ) ) {
-        $queryerrors .= '' . 'Line ' . $querycount . ' - ' . $mysqli->error . '<br>';
-        continue;
-      }
-    }
+		if ( $query == '' ) {
+			continue;
+		}
 
 
-    if ( $queryerrors ) {
-      return '' . 'There was an error on File: ' . $filename . '<br>' . $queryerrors;
-    }
-    
-    if( $mysqli && ! $mysqli->error ) {
-      @$mysqli->close();
-    }   
+		if ( ! $mysqli->query( $query ) ) {
+			$queryerrors .= 'Line ' . $querycount . ' - ' . $mysqli->error . '<br />';
+			continue;
+		}
+	}
 
-    return 'complete dumping database !';
+
+	if ( $queryerrors ) {
+		return 'There was an error on File: ' . $filename . '<br />' . $queryerrors;
+	}
+	
+	if( $mysqli && ! $mysqli->error ) {
+		@$mysqli->close();
+	}   
+
+	return 'Complete dumping database!';
 }
+
+
 
 /**
  * MYSQL EXPORT TO GZIP 
@@ -99,65 +100,65 @@ function mysqli_import_sql( $args , $dbhost, $dbuser, $dbpass ,$dbname ) {
  *
  */
 function backup_database( $directory, $outname , $dbhost, $dbuser, $dbpass ,$dbname ) {
-  
-  // check mysqli extension installed
-  if( ! function_exists('mysqli_connect') ) {
-    die(' This scripts need mysql extension to be running properly ! please resolve!!');
-  }
+	
+	// check mysqli extension installed
+	if( !function_exists('mysqli_connect') ) {
+		die('This scripts need mysql extension to be running properly! Please resolve!');
+	}
 
-  $mysqli = @new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-  
-  if( $mysqli->connect_error ) {
-    print_r( $mysqli->connect_error );
-    return false;
-  }
+	$mysqli = @new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+	
+	if ( $mysqli->connect_error ) {
+		print_r( $mysqli->connect_error );
+		return false;
+	}
 
-    $dir = $directory;
-    $result = '<p> Could not create backup directory on :'.$dir.' Please Please make sure you have set Directory on 755 or 777 for a while.</p>';  
-    $res = true;
-    if( ! is_dir( $dir ) ) {
-      if( ! @mkdir( $dir, 755 )) {
-        $res = false;
-      }
-    }
+	$dir = $directory;
+	$result = '<p>Could not create backup directory on :'.$dir.' Please Please make sure you have set Directory on 755 or 777 for a while.</p>';  
+	$res = true;
+	if( !is_dir( $dir ) ) {
+		if( !@mkdir( $dir, 755 ) ) {
+			$res = false;
+		}
+	}
 
-  $n = 1;
-  if( $res ) {
+	$n = 1;
+	if ( $res ) {
 
-    $name     = $outname;
-    # counts
-    if( file_exists($dir.'/'.$name.'.sql.gz' ) ) {
+		$name = $outname;
+		# counts
+		if ( file_exists($dir.'/'.$name.'.sql' ) ) {
 
-      for($i=1;@count( file($dir.'/'.$name.'_'.$i.'.sql.gz') );$i++){
-        $name = $name;
-        if( ! file_exists( $dir.'/'.$name.'_'.$i.'.sql.gz') ) {
-          $name = $name.'_'.$i;
-          break;
-        }
-      }
-    }
+			for ($i=1; @count( file($dir.'/'.$name.'_'.$i.'.sql') ); $i++) {
+				$name = $name;
+				if ( !file_exists( $dir.'/'.$name.'_'.$i.'.sql') ) {
+					$name = $name.'_'.$i;
+					break;
+				}
+			}
+		}
 
-    $fullname = $dir.'/'.$name.'.sql.gz'; # full structures
+		$fullname = $dir.'/'.$name.'.sql'; # full structures
 
-    if( ! $mysqli->error ) {
-      $sql = "SHOW TABLES";
-      $show = $mysqli->query($sql);
-      while ( $r = $show->fetch_array() ) {
-        $tables[] = $r[0];
-      }
+		if ( !$mysqli->error ) {
+			$sql = "SHOW TABLES";
+			$show = $mysqli->query($sql);
+			while ( $r = $show->fetch_array() ) {
+				$tables[] = $r[0];
+			}
 
-      if( ! empty( $tables ) ) {
+			if ( !empty( $tables ) ) {
 
-  //cycle through
-  $return = '';
-  foreach( $tables as $table )
-  {
-    $result     = $mysqli->query('SELECT * FROM '.$table);
-    $num_fields = $result->field_count;
-    $row2       = $mysqli->query('SHOW CREATE TABLE '.$table );
+	// Cycle through
+	$return = '';
+	foreach ( $tables as $table )
+	{
+		$result     = $mysqli->query('SELECT * FROM '.$table);
+		$num_fields = $result->field_count;
+		$row2       = $mysqli->query('SHOW CREATE TABLE '.$table );
 
-    $row2       = $row2->fetch_row();
-    $return    .= 
+		$row2       = $row2->fetch_row();
+		$return    .= 
 "\n
 -- ---------------------------------------------------------
 --
@@ -167,68 +168,67 @@ function backup_database( $directory, $outname , $dbhost, $dbuser, $dbpass ,$dbn
 
 ".$row2[1].";\n";
 
-    for ($i = 0; $i < $num_fields; $i++) 
-    {
+		for ($i = 0; $i < $num_fields; $i++) 
+		{
 
-      $n = 1 ;
-      while( $row = $result->fetch_row() )
-      { 
-        
+			$n = 1 ;
+			while ( $row = $result->fetch_row() )
+			{ 
+				
 
-        if( $n++ == 1 ) { # set the first statements
-          $return .= 
+				if ( $n++ == 1 ) { # set the first statements
+					$return .= 
 "
 --
 -- Dumping data for table `{$table}`
 --
 
 ";  
-        /**
-         * Get structural of fields each tables
-         */
-        $array_field = array(); #reset ! important to resetting when loop 
-         while( $field = $result->fetch_field() ) # get field
-        {
-          $array_field[] = '`'.$field->name.'`';
-          
-        }
-        $array_f[$table] = $array_field;
-        // $array_f = $array_f;
-        # endwhile
-        $array_field = implode(', ', $array_f[$table]); #implode arrays
+				/**
+				 * Get structural of fields each tables
+				 */
+				$array_field = array(); #reset ! important to resetting when loop 
+				while ( $field = $result->fetch_field() ) # get field
+				{
+					$array_field[] = '`'.$field->name.'`';
+					
+				}
+				$array_f[$table] = $array_field;
+				// $array_f = $array_f;
+				# endwhile
+				$array_field = implode(', ', $array_f[$table]); #implode arrays
 
-          $return .= "INSERT INTO `{$table}` ({$array_field}) VALUES\n(";
-        } else {
-          $return .= '(';
-        }
-        for($j=0; $j<$num_fields; $j++) 
-        {
-          
-          $row[$j] = str_replace('\'','\'\'', preg_replace("/\n/","\\n", $row[$j] ) );
-          if ( isset( $row[$j] ) ) { $return .= is_numeric( $row[$j] ) ? $row[$j] : '\''.$row[$j].'\'' ; } else { $return.= '\'\''; }
-          if ($j<($num_fields-1)) { $return.= ', '; }
-        }
-          $return.= "),\n";
-      }
-      # check matching
-      @preg_match("/\),\n/", $return, $match, false, -3); # check match
-      if( isset( $match[0] ) )
-      {
-        $return = substr_replace( $return, ";\n", -2);
-      }
+					$return .= "INSERT INTO `{$table}` ({$array_field}) VALUES\n(";
+				} else {
+					$return .= '(';
+				}
+				for ($j=0; $j<$num_fields; $j++) 
+				{
+					
+					$row[$j] = str_replace('\'','\'\'', preg_replace("/\n/","\\n", $row[$j] ) );
+					if ( isset( $row[$j] ) ) { $return .= is_numeric( $row[$j] ) ? $row[$j] : '\''.$row[$j].'\'' ; } else { $return.= '\'\''; }
+					if ( $j<($num_fields-1) ) { $return.= ', '; }
+				}
+					$return.= "),\n";
+			}
+			# check matching
+			@preg_match("/\),\n/", $return, $match, false, -3); # check match
+			if ( isset( $match[0] ) )
+			{
+				$return = substr_replace( $return, ";\n", -2);
+			}
 
-    }
-    
-      $return .= "\n";
+		}
+		
+			$return .= "\n";
 
-  }
+	}
 
 $return = 
 "-- ---------------------------------------------------------
 --
--- SIMPLE SQL Dump
+-- Simple SQL Dump
 -- 
--- http://www.nawa.me/
 --
 -- Host Connection Info: ".$mysqli->host_info."
 -- Generation Time: ".date('F d, Y \a\t H:i A ( e )')."
@@ -252,47 +252,30 @@ SET time_zone = \"+00:00\";
 
 # end values result
 
-    @ini_set('zlib.output_compression','Off');
-    $gzipoutput = gzencode( $return, 9);
 
- if(  @ file_put_contents( $fullname, $gzipoutput  ) ) { # 9 as compression levels
-  
-    $result = $name.'.sql.gz'; # show the name
-  
-  } else { # if could not put file , automaticly you will get the file as downloadable
+				if( @file_put_contents( $fullname, $return ) ) { # 9 as compression levels
+				
+					$result = $name.'.sql'; # show the name
+				
+				} else {
+					$result = '<p>Error when saving the export.</p>';
+				}
 
-    $result = false;   
-    // various headers, those with # are mandatory
-    header('Content-Type: application/x-download');
-    header("Content-Description: File Transfer");
-    header('Content-Encoding: gzip'); #
-    header('Content-Length: '.strlen( $gzipoutput ) ); #
-    header('Content-Disposition: attachment; filename="'.$name.'.sql.gz'.'"');
-    header('Cache-Control: no-cache, no-store, max-age=0, must-revalidate');
-    header('Connection: Keep-Alive');
-    header("Content-Transfer-Encoding: binary");
-    header('Expires: 0');
-    header('Pragma: no-cache');
-    
-    echo $gzipoutput;
+			} else {
 
-  }
+				$result = '<p>Error when executing database query to export.</p>'.$mysqli->error;
+			 
+			}
+		}
 
-       } else {
-
-         $result = '<p>Error when executing database query to export.</p>'.$mysqli->error;
-       
-       }
-     }
-
- } else {
-      $result = '<p>Wrong mysqli input</p>';
- }
+	} else {
+		$result = '<p>Wrong mysqli input</p>';
+	}
  
- if( $mysqli && ! $mysqli->error ) {
-      @$mysqli->close();
- }
+	if( $mysqli && ! $mysqli->error ) {
+		@$mysqli->close();
+	}
 
-  return $result;
+	return $result;
 
 }
