1c54e690dc2bc87f58c7bcbfaccff9ca5665f88d
Aurel Hermand <github@devone.de>
2015-04-06 16:53:49 +0200

    drop all tables before restoring

--- tools/db/database.func.php
+++ tools/db/database.func.php
@@ -31,6 +31,16 @@ function mysqli_import_sql( $args , $dbhost, $dbuser, $dbpass ,$dbname ) {
 		return false;
 	}
 
+	// DROP all tables (be carefull...)
+	$mysqli->query('SET foreign_key_checks = 0');
+	if ( $result = $mysqli->query("SHOW TABLES") ) {
+	    while ( $row = $result->fetch_array(MYSQLI_NUM) ) {
+	        $mysqli->query('DROP TABLE IF EXISTS '.$row[0]);
+	    }
+	}
+	$mysqli->query('SET foreign_key_checks = 1');
+
+
 	$querycount = 11;
 	$queryerrors = '';
 	$lines = (array) $args;
