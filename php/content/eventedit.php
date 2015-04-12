bdfa835b0b1c1a5bb84206c74261ace7a9889837
Benjamin Hermand <arjuna@Benjamins-MacBook-Air.local>
2015-04-12 14:28:44 +0200

    vorläufiges Formular

---
+++ php/content/eventedit.php
@@ -0,0 +1,11 @@
+
+
+
+<form name="demologin" id="eventadd" method="post" action="">
+	<input type="hidden" name="action" value="eventadd" />
+	<input type="text" name="title" maxlength="255" required placeholder="Titel" value="" />
+	<textarea name="description" maxlength="1023" placeholder="Beschreibung..."></textarea>
+	<input type="text" name="keywords" maxlength="1023" placeholder="keyword A, keyword B,..." value="" />
+	<select></select>
+	<input type="submit" class="demo" value="Senden" />
+</form>	
