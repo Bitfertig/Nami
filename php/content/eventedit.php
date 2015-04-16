543870d5393df747e59360e63de7c0cf04214e7f
Aurel Hermand <github@devone.de>
2015-04-17 01:08:20 +0200

    event-data & gedanken zu dates

--- php/content/eventedit.php
+++ php/content/eventedit.php
@@ -1,31 +1,79 @@
 
 <form name="demologin" id="eventedit" method="post" action="">
 	<input type="hidden" name="action" value="eventedit" />
-	<input type="hidden" name="id" value="<?=req('id')?>" />
+	<input type="hidden" name="id" value="<?=req('id', 'GET')?>" />
+
+<?php
+$userid = $_SESSION['User']->get('id');
+$eventid = req('id', 'GET');
+
+//
+function reqorglob($reqname) {
+	if ( isset($_POST['action']) ) {
+		return req($reqname);
+	}
+	return isset($GLOBALS[$reqname]) ? $GLOBALS[$reqname] : '';
+}
+
+if ( !empty($eventid) ) {
+	$sql = 'SELECT * FROM events WHERE userid = :userid AND id = :eventid';
+	$stmt = $pdo->prepare($sql);
+	$stmt->bindParam(':userid', $userid, PDO::PARAM_INT);
+	$stmt->bindParam(':eventid', $eventid, PDO::PARAM_INT);
+	$stmt->execute();
+
+	$count = $stmt->rowCount();
+
+	if ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+		$GLOBALS['id'] = $row->id;
+		$GLOBALS['title'] = $row->title;
+		$GLOBALS['description'] = $row->description;
+		$GLOBALS['keywords'] = $row->keywords;
+		$GLOBALS['status'] = $row->keywords;
+	}
+}
+?>
 
 	<h1>Veranstaltung editieren</h1>
 
 
 	<h2>Veranstaltung</h2>
 	<div>
-		<input type="text" name="title" maxlength="255" required placeholder="Titel" value="" />
+		<input type="text" name="title" maxlength="255" required placeholder="Titel" value="<?=htmlspecialchars(reqorglob('title'))?>" />
 	</div><br />
 	<div>
-		<textarea name="description" maxlength="1023" placeholder="Beschreibung..."></textarea>
+		<textarea name="description" maxlength="1023" placeholder="Beschreibung..."><?=htmlspecialchars(reqorglob('description'))?></textarea>
 	</div><br />
 	<div>
-		<input type="text" name="keywords" maxlength="1023" placeholder="keyword A, keyword B,..." value="" />
+		<input type="text" name="keywords" maxlength="1023" placeholder="Schlüsselwort A, Schlüsselwort B,..." value="<?=htmlspecialchars(reqorglob('keywords'))?>" />
 	</div><br />
 	<div>
-		<select>
-			
-		</select>
+		Bild
 	</div></br>
 
 
 	<!-- Dates -->
 	<h2>Veranstaltung-Dates</h2>
 	...
+
+<?php
+/*
+http://www.nncron.ru/help/EN/working/cron-format.htm
+
+* * * * * *
+| | | | | | 
+| | | | | +-- Year              (range: 1900-3000)
+| | | | +---- Day of the Week   (range: 1-7, 1 standing for Monday)
+| | | +------ Month of the Year (range: 1-12)
+| | +-------- Day of the Month  (range: 1-31)
+| +---------- Hour              (range: 0-23)
++------------ Minute            (range: 0-59)
+
+List: 0,1,2
+Range: 1-5
+Interval: *./4 0-23/2
+*/
+?>
 
 
 	<div>
