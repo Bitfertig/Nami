ad4bb2e221338359b9aba0be8006a33bd60bbc40
Benjamin Hermand <arjuna@Benjamins-MacBook-Air.local>
2015-05-01 23:27:46 +0200

    medien - Bilder hochladen

--- php/content/medien.php
+++ php/content/medien.php
@@ -1,8 +1,89 @@
 <h1>Medien</h1>
 
+<form enctype="multipart/form-data" action="?mode=medien" method="post">
+	<input type="hidden" name="action" value="mediaupload" />
+    <input name="userfile" type="file" />
+    <input type="submit" value="Datei abschicken" />
+</form>
+
 
 
 <?php
+// Datei hochladen
+
+
+if( !isset( $_POST['action'] ) ){
+	$_POST['action'] = '';
+}
+$action = $_POST['action'];
+
+if( $action == 'mediaupload' ){
+
+
+	$whitelist = array(
+		'image/gif',
+		'image/jpeg',
+		'image/png',
+		'image/svg+xml'
+	);
+	$extensions = array(
+		'gif',
+		'jpeg',
+		'jpg',
+		'png',
+		'svg'
+	);
+
+	// Dateiendung
+	$path_parts = pathinfo( $_FILES['userfile']['name'] );
+	$extension = $path_parts['extension'];
+
+$errors = array();
+
+	if( !in_array($_FILES['userfile']['type'], $whitelist) ){
+		$errors[] = 'mimetype';
+	}
+	if( !in_array( $extension, $extensions ) ){
+		$errors[] = 'extension';
+	}
+	if( $_FILES['userfile']['size'] > 5*1024*1024 ){    // 5mb Dateigröße
+		$errors[] = 'size';
+	}
+
+	// Bedingungen erfüllt, Datei wird gespeichert
+	if( count($errors) == 0 ){
+		//Datenbankeintrag
+		$sql = 'INSERT INTO eventfiles (userid, name) VALUES (:userid, :name)';
+		$stmt = $pdo->prepare($sql);
+		$userid = $_SESSION['User']->get('id');
+		$stmt->bindParam(':userid', $userid, PDO::PARAM_INT);
+		$stmt->bindParam(':name', $_FILES['userfile']['name'], PDO::PARAM_STR);
+		$stmt->execute();
+		$lastId = $pdo->lastInsertId();
+
+		//Datei erstellen/verschieben
+		move_uploaded_file ( $_FILES['userfile']['tmp_name'], $_SERVER['DOCUMENT_ROOT'].'/media/'
+			.$lastId.'-'.$_FILES['userfile']['name'] );
+	}
+
+	
+
+
+
+
+
+	echo $_FILES['userfile']['name'];
+
+	echo $_FILES['userfile']['tmp_name'];
+
+	// $_FILES['userfile']['error']
+}
+
+
+?>
+
+<?php
+
 
 $userid = $_SESSION['User']->get('id');
 
@@ -12,9 +93,22 @@ $stmt = $pdo->prepare($sql);
 $stmt->bindParam(':userid', $userid, PDO::PARAM_INT);
 $stmt->execute();
 
+
+
+echo '<ul>';
 while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
-    echo $row->id .' '. $row->filename .' '. $row->extension .'<br />';
+    echo '<li>'.$row->id .' '. $row->name .'</li>';
 }
+echo '</ul>';
 
 
 ?>
+
+
+
+
+
+
+
+
+
---
+++ tools/db/backups/backup_2015-05-01-22-50-31.sql
@@ -0,0 +1,131 @@
+-- ---------------------------------------------------------
+--
+-- Simple SQL Dump
+-- 
+--
+-- Host Connection Info: Localhost via UNIX socket
+-- Generation Time: May 01, 2015 at 22:50 PM ( Europe/Berlin )
+-- Server version: 5.5.38
+-- PHP Version: 5.6.2
+--
+-- ---------------------------------------------------------
+
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+
+-- ---------------------------------------------------------
+--
+-- Table structure for table : `eventdates`
+--
+-- ---------------------------------------------------------
+
+CREATE TABLE `eventdates` (
+  `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
+  `eventid` int(20) unsigned NOT NULL,
+  `startdate` varchar(255) NOT NULL COMMENT 'Cron Format',
+  `duration` int(100) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `eventdates`
+--
+
+INSERT INTO `eventdates` (`id`, `eventid`, `startdate`, `duration`) VALUES
+(1, 1, '10 9 * * 1-5 *', 3600);
+
+
+
+-- ---------------------------------------------------------
+--
+-- Table structure for table : `eventfiles`
+--
+-- ---------------------------------------------------------
+
+CREATE TABLE `eventfiles` (
+  `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
+  `userid` int(20) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `eventfiles`
+--
+
+INSERT INTO `eventfiles` (`id`, `userid`, `name`) VALUES
+(1, 1, '9ga.png');
+
+
+
+-- ---------------------------------------------------------
+--
+-- Table structure for table : `events`
+--
+-- ---------------------------------------------------------
+
+CREATE TABLE `events` (
+  `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
+  `status` int(1) NOT NULL,
+  `userid` int(20) unsigned NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(1023) NOT NULL,
+  `keywords` varchar(1023) NOT NULL,
+  `eventfileid` int(20) NOT NULL COMMENT 'Ein Bild',
+  `lastchange` int(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`id`, `status`, `userid`, `title`, `description`, `keywords`, `eventfileid`, `lastchange`) VALUES
+(1, 1, 1, 'Meine <b>Veranstaltung</b> "1"', 'Die und das ist die Beschreibung.', 'Wort1, Wort2, Wort3', 0, 0),
+(2, 0, 1, 'Meine Veranstaltung 2', 'Die und das ist die Beschreibung.', 'Wort1, Wort2, Wort3', 0, 0);
+
+
+
+-- ---------------------------------------------------------
+--
+-- Table structure for table : `users`
+--
+-- ---------------------------------------------------------
+
+CREATE TABLE `users` (
+  `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
+  `status` int(1) NOT NULL DEFAULT '0',
+  `username` varchar(20) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `birthday` int(20) unsigned DEFAULT NULL,
+  `registertime` int(20) unsigned NOT NULL,
+  `registercode` varchar(9) NOT NULL,
+  `lastlogin` int(20) NOT NULL,
+  `ip` varchar(20) NOT NULL,
+  `resetcode` varchar(9) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `status`, `username`, `password`, `email`, `birthday`, `registertime`, `registercode`, `lastlogin`, `ip`, `resetcode`) VALUES
+(1, 1, 'dipser', '0fdd9f682fe6d949234730348710ac7a', 'dipser@gmail.com', 0, 1428191169, '17RdTz7O9', 0, '127.0.0.1', ''),
+(2, 1, 'Benjamin', '0fdd9f682fe6d949234730348710ac7a', 'benjamin@hermand.de', 0, 1428340739, 'wpWheMkNE', 0, '127.0.0.1', ''),
+(3, -1, 'Demobenutzer', 'd2d64ceaf36b04edc58ef31de7cde598', 'user@demo', 0, 1428340992, 'NNdRAoLJo', 0, '127.0.0.1', '');
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
